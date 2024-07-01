fe844d660a7ecf5f8e0b444ca1b09946e6adf96f
JAYESHPATEL24 <jayesh.p.patel24@gmail.com>
2024-07-01 22:00:09 +0530

    Assignment

---
+++ Assignment/Module 3 SE – Fundamentals of Programming/1.Basic Logic Program/01. use of printf.c
@@ -0,0 +1,11 @@
+/*	Display This Information using printf
+	a.Your Name
+	b.Your Birth date
+	c.Your Age
+	d.Your Address */
+
+#include<stdio.h>
+main()
+{
+	printf("a.Your Name \nb.Your Birth date \nc.Your Age \nd.Your Address");
+}
