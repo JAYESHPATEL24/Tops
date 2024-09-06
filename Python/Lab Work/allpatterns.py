204edcbc087e0e029533d3d1670a5531fa089b8e
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-06 10:14:37 +0530

    Class Work

--- Python/Lab Work/allpatterns.py
+++ Python/Lab Work/allpatterns.py
@@ -45,7 +45,7 @@ print()
 for i in range(5):
     for j in range(1, 5-i):
         print(" ", end="")
-    for k in range(0, i + 1):
+    for k in range(i + 1):
         time.sleep(0.3)
         print("*", end="")
     print()
