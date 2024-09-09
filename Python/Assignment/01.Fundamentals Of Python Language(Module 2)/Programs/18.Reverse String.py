9aafa59b073a8083a6cbbb30373b62eb5e49f3aa
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-09 15:03:32 +0530

    Assignment

--- Python/Assignment/01.Fundamentals Of Python Language(Module 2)/Programs/18.Reverse String.py
+++ Python/Assignment/01.Fundamentals Of Python Language(Module 2)/Programs/18.Reverse String.py
@@ -1,1 +1,14 @@
 # Write a Python function to reverses a string if its length is a multiple of 4.  
+
+s = input("Enter a string: ")
+
+        # Check if the length of the string is a multiple of 4
+if len(s) % 4 == 0:
+            # Reverse the string
+    rs = s[::-1]
+else:
+        # If length is not multiply by 4, keep the string as it is
+    rs = s
+
+    # Print the rs
+print(f"The result is: {rs}")
