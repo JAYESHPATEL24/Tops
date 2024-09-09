7d86885aa4f1f48cee0b4abeee3b6a27084af958
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-09 14:49:04 +0530

    Assignment

--- Python/Assignment/01.Fundamentals Of Python Language(Module 2)/Programs/20.String insertion.py
+++ Python/Assignment/01.Fundamentals Of Python Language(Module 2)/Programs/20.String insertion.py
@@ -1,1 +1,17 @@
 # Write a Python function to insert a string in the middle of a string. 
+
+        #main string
+main_s = input("Enter the main string: ")
+
+        # take user input to enter the string to insert
+s = input("Enter the string to insert: ")
+
+        # Calculate the middle index of the main string
+middle = len(main_s) // 2
+
+
+        # Insert the string at the middle index
+result = main_s[:middle] + s + main_s[middle:]
+
+        # Print the result
+print(f"The new string is: {result}")
