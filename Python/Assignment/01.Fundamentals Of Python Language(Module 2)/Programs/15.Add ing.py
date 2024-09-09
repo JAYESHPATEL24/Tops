1d8c88056ac5a99c6ab245d5debd7880784a2faa
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-09 15:23:42 +0530

    Assignment

--- Python/Assignment/01.Fundamentals Of Python Language(Module 2)/Programs/15.Add ing.py
+++ Python/Assignment/01.Fundamentals Of Python Language(Module 2)/Programs/15.Add ing.py
@@ -1,3 +1,21 @@
 # Write a Python program to add 'ing' at the end of a given string (length should be at least 3). 
 # If the given string already ends with 'ing' then add 'ly' instead 
 # if the string length of the given string is less than 3, leave it unchanged. 
+
+s = input("Enter a String : ")
+
+    # Check the length of the string
+if len(s) >= 3:
+    # Check if the string ends with 'ing'
+    if s.endswith('ing'):
+             # Add 'ly' if the string ends with 'ing'
+        s1 = s + 'ly'
+    else:
+        # Add 'ing' if the string does not end with 'ing'
+        s1 = s + 'ing'
+else:
+    # Leave the string unchanged if its length is less than 3
+    s1 = s
+
+# Print the resulting string
+print(f"The resulting string is: {s1}")
