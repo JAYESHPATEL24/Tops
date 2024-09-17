14714f46ed5341c73c03508459ca8bf8a5ca414e
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-17 20:42:58 +0530

    Assignment

--- Python/Assignment/01.Fundamentals Of Python Language(Module 2)/Programs/17.Length of longest word.py
+++ Python/Assignment/01.Fundamentals Of Python Language(Module 2)/Programs/17.Length of longest word.py
@@ -1,8 +1,6 @@
 # Write a Python function that takes a list of words and returns the length of the longest one.
 
-def longest_word_length():
-        #user input
-    words = input("Enter a list of words separated by spaces: ").split()
+def longest_word_length(words):
     
         # variable to store the length of the longest word
     l = 0
@@ -13,7 +11,12 @@ def longest_word_length():
             l = len(i)
     return l
 
+
+        #user input
+words = input("Enter a list of words separated by spaces: ").split()
         #function call
-print(f"ther longest length of word from words is {longest_word_length()}.")
+print(f"ther longest length of word from words is {longest_word_length(words)}.")
+
+
 
 
