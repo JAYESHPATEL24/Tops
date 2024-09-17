f9c28950a14d790463ae7f88dd5edccbb35db214
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-17 20:34:20 +0530

    Assignment

--- Python/Assignment/01.Fundamentals Of Python Language(Module 2)/Programs/16. not poor to good.py
+++ Python/Assignment/01.Fundamentals Of Python Language(Module 2)/Programs/16. not poor to good.py
@@ -4,13 +4,13 @@
 s = input("Enter a string: ")
 
     # Find the first occurrence of 'not' and 'poor'
-s1 = s.find('not')
-s2 = s.find('poor')
+s1 = s.find("not")
+s2 = s.find("poor")
 
     # Check if 'not' appears before 'poor'
 if s1 != -1 and s2 != -1 and s1 < s2:
         # Replace the 'not'...'poor' substring with 'good'
-    result = s[:s1] + 'good' + s[s2 + 4:]
+    result = s[:s1] + "good" + s[s2 + 4:]
 else:
         # If the condition is not met, return the original string
     result = s
