3306af3b49a1cba495614191d6be6ab28ba0140d
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-23 12:22:41 +0530

    Assignment

--- Python/Assignment/01.Fundamentals Of Python Language(Module 2)/Programs/06.Vowel or not.py
+++ Python/Assignment/01.Fundamentals Of Python Language(Module 2)/Programs/06.Vowel or not.py
@@ -1,5 +1,6 @@
 #  Write a Python program to test whether a passed letter is a vowel or not.  
 
+"""
 vowels = "aeiouAEIOU"       # Define a string containing all vowels (both lowercase and uppercase)
 
 letter = input("Enter any letter : ")
@@ -8,3 +9,12 @@ if letter in vowels:        # Check if the entered letter is in the string of vo
     print(f"{letter} is Vowel.")
 else:
     print(f"{letter} is not Vowel.")
+    
+"""
+
+l = input("Enter any letter : ")
+
+if l == 'a' or l == 'e' or l == 'i' or l == 'o' or l == 'u' or l == 'A' or l == 'E' or l == 'I' or l == 'O' or l == 'U':
+     print(f"{l} is Vowel.")
+else:
+    print(f"{l} is not Vowel.")
---
+++ Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/01.Largest, Smallest and Sum.py
@@ -0,0 +1,4 @@
+# Write a Python function to get the largest number, smallest num and sum of all from a list. 
+
+def large_small_sum():
+    print("Hello")
