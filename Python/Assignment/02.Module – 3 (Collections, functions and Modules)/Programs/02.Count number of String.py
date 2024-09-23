2d5a967ecceced9322ac2734c0749871a0066d5d
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-23 15:40:45 +0530

    Assignment

--- Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/02.Count number of String.py
+++ Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/02.Count number of String.py
@@ -1,16 +1,19 @@
 # Write a Python program to count the number of strings where the string length is 2 or more and the first 
 # and last character are same from a given list of strings.  
 
+    # user input
 n = int(input("How many strings you wants to enter ? : "))
 
 l = [] 
 
+    # add user inputs into list.
 for i in range(n):
     s = input("Enter a String : ")
     l.append(s) 
     
 count = 0 
 
+    # count strings where the string length is 2 or more and the first and last character are same from a given list of strings. 
 for i in l:
     if len(i) > 2 and i[0] == i[-1]:
           count += 1
--- Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/03.Remove Duplicates.py
+++ Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/03.Remove Duplicates.py
@@ -1,20 +1,25 @@
 # Write a Python program to remove duplicates from a list.  
 
+    # User input for list length
 n = int(input("Enter a length of list: "))
 
 l = []
 
+    # User inputs for list
 for i in range(n):
     a = input("Enter an element: ")
     l.append(a)
-    
-ul = []
-
-for i in l:
-    if i not in ul:
-        ul.append(i)
 
 print(f"Original list : {l} ")
-print(f"List after removing duplicates : {ul}")
+    # Remove duplicates from the original list
+i = 0
+while i < len(l):
+    if l.count(l[i]) > 1:
+        l.remove(l[i])
+    else:
+        i += 1
+        
+    # print list after removing duplicates elements. 
+print(f"List after removing duplicates: {l}")
 
 
