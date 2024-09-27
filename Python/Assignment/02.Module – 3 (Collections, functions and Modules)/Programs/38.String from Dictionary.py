d712d66bb32e24c36b073d0cf1e8e125b1ee8b6c
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-27 13:20:53 +0530

    Assignment

--- Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/38.String from Dictionary.py
+++ Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/38.String from Dictionary.py
@@ -3,5 +3,5 @@
 Note: Track the count of the letters from the string.  
 Sample string: 'w3resource'  
 Expected output:  
-{'3': 1,’s’: 1, 'r': 2, 'u': 1, 'w': 1, 'c': 1, 'e': 2, 'o': 1}
+{'3': 1,'s': 1, 'r': 2, 'u': 1, 'w': 1, 'c': 1, 'e': 2, 'o': 1}
 """  
