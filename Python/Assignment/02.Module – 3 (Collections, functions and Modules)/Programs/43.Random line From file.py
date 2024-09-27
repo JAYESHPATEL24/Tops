3db4b93f0e057e3177676fde94f330119744781f
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-27 14:33:37 +0530

    Assignment

--- Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/43.Random line From file.py
+++ Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/43.Random line From file.py
@@ -1,4 +1,14 @@
 # Write a Python program to read a random line from a file. 
 
+import random
+
+file_name = 'random.txt'
+
+with open(file_name, 'r+') as file:
+    lines = file.readlines()
+    random_line = random.choice(lines).strip()
+
+print(random_line)
+
 
 
