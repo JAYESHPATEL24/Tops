87f55d1e483ff62918390603249b8b3a7357bce5
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-27 16:05:34 +0530

    Assignment

--- Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/43.Random line From file.py
+++ Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/43.Random line From file.py
@@ -2,13 +2,13 @@
 
 import random
 
-file_name = 'random.txt'
 
-with open(file_name, 'r+') as file:
-    lines = file.readlines()
-    random_line = random.choice(lines).strip()
+    # open the file in read mode
+file  = open('C:\Users\jayes\OneDrive\Documents\MY CUTIPIE.txt', 'r')
+    # Read all line into a List
+lines = file.readlines()
+    # Choose a random line from the list.
+random_line = random.choice(lines) # strip my leading/trailing whitespace
 
-print(random_line)
-
-
-
+print("Random line  : ")
+print(random_line.strip())
