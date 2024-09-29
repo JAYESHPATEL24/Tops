c17972a75341448a5762e75148e1d780673e6f3e
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-29 21:32:57 +0530

    Assignment

--- Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/43.Random line From file.py
+++ Python/Assignment/02.Module – 3 (Collections, functions and Modules)/Programs/43.Random line From file.py
@@ -1,14 +1,18 @@
-# Write a Python program to read a random line from a file. 
+# Write a Python program to read a random line from a file.
 
 import random
 
+    # Open the file in read mode
+file = open("Hearme.txt", 'r')
 
-    # open the file in read mode
-file  = open('C:\Users\jayes\OneDrive\Documents\MY CUTIPIE.txt', 'r')
-    # Read all line into a List
+    # Read all lines into a list
 lines = file.readlines()
-    # Choose a random line from the list.
-random_line = random.choice(lines) # strip my leading/trailing whitespace
 
-print("Random line  : ")
-print(random_line.strip())
+    # Check if the list is not empty
+if lines:
+    # Choose a random line from the list
+    random_line = random.choice(lines)
+    print("Random line : ", random_line)  # Print the line without trailing newline
+else:
+    print("The file is empty.")
+
