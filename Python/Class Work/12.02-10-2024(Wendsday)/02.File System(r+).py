3b5cf7fb0288f90adc91d375891486ab433e1b17
JAYESH <jayesh.p.patel24@gmail.com>
2024-10-02 09:26:24 +0530

    Class Work

---
+++ Python/Class Work/12.02-10-2024(Wendsday)/02.File System(r+).py
@@ -0,0 +1,5 @@
+with open("C:\\Users\\jayes\\Documents\\Tops\\Tops\\Python\\Class Work\\12.02-10-2024(Wendsday)\\Good.txt","r+") as file:
+    print(file.readline())
+    file.write("Hello Beautiful girl!!!!!!")
+    
+    
---
+++ Python/Class Work/12.02-10-2024(Wendsday)/04.math.py
@@ -0,0 +1,5 @@
+import math
+
+print(math.sqrt(144))
+
+print(math.pow(10))
