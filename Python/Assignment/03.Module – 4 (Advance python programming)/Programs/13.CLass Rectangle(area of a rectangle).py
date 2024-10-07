4a3b0b478eea3edcd84781ea51ef8d0097b84590
JAYESH <jayesh.p.patel24@gmail.com>
2024-10-07 11:02:28 +0530

    Assignment

--- Python/Assignment/03.Module – 4 (Advance python programming)/Programs/13.CLass Rectangle(area of a rectangle).py
+++ Python/Assignment/03.Module – 4 (Advance python programming)/Programs/13.CLass Rectangle(area of a rectangle).py
@@ -1,1 +1,22 @@
 # Write a Python class named Rectangle constructed by a length and width and a method which will compute the area of a rectangle.
+
+class Rectangle:
+    def __init__(self, length, width):
+            # Initialize the length and width attributes
+        self.length = length 
+        self.width = width
+
+    def area(self):
+            # Calculate and print the area of the rectangle
+        print(f"Area of a Rectangle : {self.width * self.length}")
+
+
+    # get length and width from the user.
+l = int(input("Enter a Length : "))
+w = int(input("Enter a Width  : "))
+
+    # Create an object of class.
+obj = Rectangle(l, w)
+
+    # Call the area method.
+obj.area()
--- Python/Assignment/03.Module – 4 (Advance python programming)/Programs/14.class Circle(area and the perimeter of a circle).py
+++ Python/Assignment/03.Module – 4 (Advance python programming)/Programs/14.class Circle(area and the perimeter of a circle).py
@@ -1,1 +1,22 @@
 # Write a Python class named Circle constructed by a radius and two methods which will compute the area and the perimeter of a circle
+
+class Circle:
+    def area(self, radius):
+            # Calculate and print the area of the circle
+        print(f"Area of Circle: {3.14 * radius * radius}")
+        
+    def perimeter(self, radius):
+            # Calculate and print the perimeter of the circle
+        print(f"Perimeter of Circle: {2 * 3.14 * radius}")
+
+    # get the radius from the users.
+r = int(input("Enter Radius of Circle: "))
+
+    # Create an instance of Circle
+obj = Circle()
+
+    # Call the area method.
+obj.area(r)
+
+    # Call the perimeter method to print the perimeter.
+obj.perimeter(r)
