58cc1d1a42801454a10b626968dc9043c7b5bb05
JAYESH <jayesh.p.patel24@gmail.com>
2024-10-14 15:01:23 +0530

    Lab Work

--- Python/Lab Work/Paranthesis List.py
+++ Python/Lab Work/Paranthesis List.py
@@ -1,3 +1,18 @@
 #  given n pairs of parentheses, write a function to generate all combination of well-formed parentheses.
 # input : n = 3
 # output : ["((()))","(()())","(())()","()(())","()()()"]
+
+n = int(input("Enter a Number : "))
+l = []
+stack = [("", 0, 0)]
+
+while stack:
+    s, left, right = stack.pop()
+    if len(s) == 2 * n:
+        l.append(s)
+    if left < n:
+        stack.append((s + "(", left + 1, right))
+    if right < left:
+        stack.append((s + ")", left, right + 1))
+
+print(l)
