6ee0c10fe2d5e8da013fcd312ffe03e7e6681c02
JAYESH <jayesh.p.patel24@gmail.com>
2024-10-18 10:25:12 +0530

    Lab Work

---
+++ Python/Lab Work/tkinter basic.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+    # Create a main Window.
+root = tk.Tk()
+root.title("Billing Software")
+
+    # create a Label
+tk.Label(root, text="Hello, Tkinter!").pack()
+color = 'Blue'
+
+    # create a Button
+tk.Button(root, text="Click Me").pack()
+
+    # Run The application
+root.mainloop()
+
+
+
