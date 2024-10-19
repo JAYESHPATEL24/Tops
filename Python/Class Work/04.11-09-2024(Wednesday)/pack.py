54ad7a8271d881d2996b25fc1a0d96a738abbaab
JAYESH <jayesh.p.patel24@gmail.com>
2024-10-19 09:12:57 +0530

    Class Work

--- Python/Class Work/04.11-09-2024(Wednesday)/pack.py
+++ Python/Class Work/04.11-09-2024(Wednesday)/pack.py
@@ -11,14 +11,14 @@ def prime(n):   # function with parameter without return type
         print(f"{n} is not a Prime Number.")
         
 
-def right():
+def right():   # function without parameter without return type
     for i in range(5):
         for j in range(i+1):
             time.sleep(0.5)
             print("\U0001F4B8",end="")
         print()
         
-def left():
+def left():     # function without parameter without return type
     for i in range(5):
         for j in range(1,5-i):
             print("  ",end="")
@@ -27,7 +27,7 @@ def left():
             print("\U0001F607",end="")
         print()
         
-def triangle():
+def triangle():    # function without parameter without return type
     for i in range(5):
         for j in range(1,5-i):
             print(" ",end="")
