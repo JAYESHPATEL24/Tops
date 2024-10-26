3c24be9180c366c0afb1c7dd302bdc4198ce5851
JAYESH <jayesh.p.patel24@gmail.com>
2024-10-26 15:00:33 +0530

    Assessment

--- Python/Assessment/02.Advance Python Programming/01.Billing.py
+++ Python/Assessment/02.Advance Python Programming/01.Billing.py
@@ -1,1 +1,19 @@
 from tkinter import *
+
+root = Tk()
+
+root.geometry("10000x10000")
+root.title("Billing Software")
+root.config(bg="white")
+
+frame = Frame(root, bg="darkblue", width=200, height=100, bd=7, )
+frame.place(x=0, y=0, relwidth=1)
+# frame.pack(padx=10, pady=10)
+
+label = Label(frame, text="Billing Software", bg="darkblue", fg="white")
+label.pack()
+
+# label =  Label(root, text="Billing Software", font=("Arial",20,"bold"), anchor="center", fg="white")
+# label.place(relheight=1, relwidth=1)
+
+root.mainloop()
