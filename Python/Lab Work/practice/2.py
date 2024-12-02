9deb19e98803b2fef578163d3e79a67e7532bb43
JAYESH <jayesh.p.patel24@gmail.com>
2024-12-02 11:00:08 +0530

    Lab Work

---
+++ Python/Lab Work/practice/2.py
@@ -0,0 +1,21 @@
+
+# def abc(x):
+#     l = []
+#     l.append(x)
+#     return l 
+
+# print(abc(1))
+# print(abc(2)) // [1][2]
+
+# print(2 ** 3 ** 2) // 512
+
+# x =10
+# def hello():
+#     x = 5
+
+# hello()
+# print(x) //10
+
+# print(3 * "abc") // abcabcabc
+
+# print("python" + 3)  // error
